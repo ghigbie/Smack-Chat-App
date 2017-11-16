d02014fc5583eb12fdb2a7129f255699bbc1136d
George Higbie <gehigbie@gmail.com>
2017-11-15 21:25:47 -0700

    Modify layout and create user activity

--- app/src/main/java/com/georgehigbie/smack/CreateUserActivity.kt
+++ app/src/main/java/com/georgehigbie/smack/CreateUserActivity.kt
@@ -2,11 +2,24 @@ package com.georgehigbie.smack
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class CreateUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun generateColorClicked(view: View){
+
+    }
+
+    fun createUserClicked(view: View){
+
     }
 }
