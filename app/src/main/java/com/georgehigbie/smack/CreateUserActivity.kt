94fc9905e0ddf6004256a62098159b91a51c9417
George Higbie <gehigbie@gmail.com>
2017-11-18 19:07:01 -0700

    Add method to generateAvatar color

--- app/src/main/java/com/georgehigbie/smack/CreateUserActivity.kt
+++ app/src/main/java/com/georgehigbie/smack/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.georgehigbie.smack
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +22,18 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateColorClicked(view: View){
+        val random = Random()
+        val redValue = random.nextInt(255)
+        val greenValue = random.nextInt(255)
+        val blueValue = random.nextInt(255)
 
+        createAvatarImageView.setBackgroundColor(Color.rgb(redValue, greenValue, blueValue)) //this sets the background color of the image
+
+        val savedRedValue = redValue.toDouble() / 255
+        val savedGreenValue = greenValue.toDouble() / 255
+        val savedBlueValue = blueValue.toDouble() /255
+
+        avatarColor = "[$savedRedValue, $savedGreenValue, $savedBlueValue]"
     }
 
     fun createUserClicked(view: View){
