20046f384f0941868b64de4e6486114f78db4f2e
George Higbie <gehigbie@gmail.com>
2017-11-20 21:20:21 -0700

    Add register user function

--- app/src/main/java/com/georgehigbie/smack/Services/AuthService.kt
+++ app/src/main/java/com/georgehigbie/smack/Services/AuthService.kt
@@ -1,7 +1,41 @@
 package com.georgehigbie.smack.Services
 
+import android.content.Context
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.georgehigbie.smack.Utilities.URL_REGISTER
+import org.json.JSONObject
+
 /**
  * Created by georgehigbie on 11/19/17.
  */
 object AuthService {
+
+    fun registerUSer(context: Context, email: String, password: String, complete:(Boolean) -> Unit){
+
+        val jsonBody = JSONObject()
+        jsonBody.put("email", email)
+        jsonBody.put("password", password)
+        val requestBody = jsonBody.toString()
+
+        val registerRequest = object: StringRequest(Method.POST, URL_REGISTER, Response.Listener { response ->
+            println(response)
+            complete(true)
+        }, Response.ErrorListener { error ->
+            Log.d("ERROR", "Could not register user: $error")
+            complete(false)
+        }){
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+        }
+
+        Volley.newRequestQueue(context).add(registerRequest)
+    }
 }
--- app/src/main/java/com/georgehigbie/smack/Utilities/Constants.kt
+++ app/src/main/java/com/georgehigbie/smack/Utilities/Constants.kt
@@ -3,3 +3,6 @@ package com.georgehigbie.smack.Utilities
 /**
  * Created by georgehigbie on 11/19/17.
  */
+
+const val BASE_URL = "https://chattycat1.herokuapp.com/v1/"
+const val URL_REGISTER = "${BASE_URL}account/register"
