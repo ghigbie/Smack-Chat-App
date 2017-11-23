9a35065589cb9f8b43e746a072bc6d459abafe53
George Higbie <gehigbie@gmail.com>
2017-11-22 21:35:28 -0700

    Add method call to create user on click event

--- app/src/main/java/com/georgehigbie/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/georgehigbie/smack/Controller/CreateUserActivity.kt
@@ -50,18 +50,24 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
+
+        val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
+
         AuthService.registerUser(this, email, password){ registerSuccess ->
             if(registerSuccess){
                 println("REGISTER COMPLETE")
                 AuthService.loginUser(this, email, password){ loginSuccess ->
                     if(loginSuccess){
-                        println(AuthService.authToken)
-                        println(AuthService.userEmail)
                         println("LOGIN COMPLETE")
+                        AuthService.createUser(this, userName, email, userAvatar, avatarColor){ createSuccess ->
+                            if(createSuccess){
+                                println("CREATE COMPLETE")
+                                println("DONE!!!!!")
+                            }
+                        }
                     }
-
                 }
             }
         }
