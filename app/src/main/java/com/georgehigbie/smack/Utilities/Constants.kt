cef8650132547a9a2cfeec010ab177e1688a788d
George Higbie <gehigbie@gmail.com>
2017-11-22 20:25:45 -0700

    Add user data service

--- app/src/main/java/com/georgehigbie/smack/Utilities/Constants.kt
+++ app/src/main/java/com/georgehigbie/smack/Utilities/Constants.kt
@@ -7,3 +7,4 @@ package com.georgehigbie.smack.Utilities
 const val BASE_URL = "https://chattycat3.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
