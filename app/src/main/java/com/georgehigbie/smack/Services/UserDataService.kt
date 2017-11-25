e5c783ffa7af2525467713cb3d41ff76189ececd
George Higbie <gehigbie@gmail.com>
2017-11-24 20:52:17 -0700

    Add return avatar color funciton

--- app/src/main/java/com/georgehigbie/smack/Services/UserDataService.kt
+++ app/src/main/java/com/georgehigbie/smack/Services/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.georgehigbie.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 /**
  * Created by georgehigbie on 11/22/17.
  */
@@ -10,5 +13,26 @@ object UserDataService {
     var avatarName = ""
     var email = ""
     var name = ""
+
+    fun retrunAvatarColor(components: String): Int{
+
+        val strippedColor = components
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+
+        var redValue = 0
+        var greenValue = 0
+        var blueValue = 0
+
+        val scanner = Scanner(strippedColor)
+        if(scanner.hasNext()){
+            redValue = (scanner.nextDouble() * 255).toInt()
+            greenValue = (scanner.nextDouble() * 255).toInt()
+            blueValue = (scanner.nextDouble()* 255).toInt()
+        }
+
+        return Color.rgb(redValue, greenValue, blueValue)
+    }
 
 }
