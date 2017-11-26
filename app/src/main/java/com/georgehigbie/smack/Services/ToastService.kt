7d04097b6b4ac72f281dc263dfd25a96d34acd3f
George Higbie <gehigbie@gmail.com>
2017-11-25 21:01:33 -0700

    Add methods to toast service

--- app/src/main/java/com/georgehigbie/smack/Services/ToastService.kt
+++ app/src/main/java/com/georgehigbie/smack/Services/ToastService.kt
@@ -1,7 +1,18 @@
 package com.georgehigbie.smack.Services
 
+import android.content.Context
+import android.widget.Toast
+
 /**
  * Created by georgehigbie on 11/25/17.
  */
 object ToastService {
+
+    fun createToastShort(context: Context, message: String){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun createToastLong(context: Context, message: String){
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
 }
