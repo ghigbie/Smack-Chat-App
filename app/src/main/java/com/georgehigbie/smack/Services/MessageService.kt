b2c34539a426408096e749c6cfeea04032bca84c
George Higbie <gehigbie@gmail.com>
2017-11-28 21:53:07 -0700

    Add message service

---
+++ app/src/main/java/com/georgehigbie/smack/Services/MessageService.kt
@@ -0,0 +1,10 @@
+package com.georgehigbie.smack.Services
+
+import com.georgehigbie.smack.Model.Channel
+
+/**
+ * Created by georgehigbie on 11/28/17.
+ */
+object MessageService {
+    val channels = ArrayList<Channel>()
+}
