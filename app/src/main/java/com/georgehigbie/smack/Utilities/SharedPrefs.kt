5065e061c707badf4aa780bf01f5b51837534a0a
George Higbie <gehigbie@gmail.com>
2017-12-02 19:23:08 -0700

    Add getters and setters for shared prefs

--- app/src/main/java/com/georgehigbie/smack/Utilities/SharedPrefs.kt
+++ app/src/main/java/com/georgehigbie/smack/Utilities/SharedPrefs.kt
@@ -1,7 +1,28 @@
 package com.georgehigbie.smack.Utilities
 
+import android.content.Context
+
 /**
  * Created by georgehigbie on 12/2/17.
  */
-class SharedPrefs {
+class SharedPrefs(context: Context) {
+
+    val PREFS_FILENAME = "prefs"
+    val prefs = context.getSharedPreferences(PREFS_FILENAME, 0)
+
+    val IS_LOGGED_IN = "isLoggedIn"
+    val AUTH_TOKEN = "authToken"
+    val USER_EMAIL = "userEmail"
+
+    var isLoggedIn: Boolean
+        get() = prefs.getBoolean(IS_LOGGED_IN, false)
+        set(value) = prefs.edit().putBoolean(IS_LOGGED_IN, value).apply()
+
+    var authToken: String
+        get() = prefs.getString(AUTH_TOKEN, "")
+        set(value) = prefs.edit().putString(AUTH_TOKEN, value).apply()
+
+    var userEmailL: String
+        get() = prefs.getString(USER_EMAIL, "")
+        set(value) = prefs.edit().putString(USER_EMAIL, value).apply()
 }
