d298c6a2b5f32652a63a0e5af465e4949b060b1c
George Higbie <gehigbie@gmail.com>
2017-12-02 19:33:05 -0700

    Add companion object and onCreate method

--- app/src/main/java/com/georgehigbie/smack/Controller/App.kt
+++ app/src/main/java/com/georgehigbie/smack/Controller/App.kt
@@ -1,10 +1,20 @@
 package com.georgehigbie.smack.Controller
 
 import android.app.Application
+import com.georgehigbie.smack.Utilities.SharedPrefs
 
 /**
  * Created by georgehigbie on 12/2/17.
  */
 class App: Application(){
+
+    companion object {
+        lateinit var sharedPreferences: SharedPrefs
+    }
+
+    override fun onCreate() {
+        sharedPreferences = SharedPrefs(applicationContext)
+        super.onCreate()
+    }
 
 }
