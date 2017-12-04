6fcda2e225f10bc23bbba018529415e7d8e78284
George Higbie <gehigbie@gmail.com>
2017-12-03 20:35:36 -0700

    Remove context

--- app/src/main/java/com/georgehigbie/smack/Services/AuthService.kt
+++ app/src/main/java/com/georgehigbie/smack/Services/AuthService.kt
@@ -17,7 +17,7 @@ import org.json.JSONObject
  */
 object AuthService {
 
-    fun registerUser(context: Context, email: String, password: String, complete:(Boolean) -> Unit){
+    fun registerUser(email: String, password: String, complete:(Boolean) -> Unit){
 
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
@@ -41,7 +41,7 @@ object AuthService {
         App.prefs.requestQueue.add(registerRequest)
     }
 
-    fun loginUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit){
+    fun loginUser(email: String, password: String, complete: (Boolean) -> Unit){
 
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
@@ -75,7 +75,7 @@ object AuthService {
         App.prefs.requestQueue.add(loginRequest)
     }
 
-    fun createUser(context: Context, name: String, email: String, avatarName: String, avatarColor: String, complete: (Boolean) -> Unit) {
+    fun createUser(name: String, email: String, avatarName: String, avatarColor: String, complete: (Boolean) -> Unit) {
 
         val jsonBody = JSONObject()
         jsonBody.put("name", name)
