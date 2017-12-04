946ea43b64b28e6b3594acedd6db63a434deaac7
George Higbie <gehigbie@gmail.com>
2017-12-03 21:28:04 -0700

    Create Message class with constructor

---
+++ app/src/main/java/com/georgehigbie/smack/Model/Message.kt
@@ -0,0 +1,8 @@
+package com.georgehigbie.smack.Model
+
+/**
+ * Created by georgehigbie on 12/3/17.
+ */
+class Message (val message: String, val userName: String, val channelId: String,
+               val userAvatar: String, val userAvatarColor: String,
+               val id: String, val timeStamp: String)
